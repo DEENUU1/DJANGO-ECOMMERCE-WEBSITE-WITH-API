1f70268947ae2f2fd73f1a57ea602023842957cf
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2022-12-16 16:32:25 +0100

    product and category models

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+# registering model category to admin
+# displaying name and slug of the product
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+# registering model product to admin
+# displaying name, slug, price, stock, available, created, update of the product
+# available works as a button YES or NO
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock',
+                    'available', 'created', 'updated']
+
+    list_filer = ['available', 'created', 'updated']
+
+    list_editable = ['price', 'stock', 'available']
+
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Product, ProductAdmin)
--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+# This class creates categories
+# And joins them products
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+# This class creates products
+# I used DecimalField here to prevent rounding prices
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
