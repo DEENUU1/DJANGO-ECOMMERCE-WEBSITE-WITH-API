fb51b2c6bb407fe05aa06b950023341a23639936
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2023-01-08 21:21:07 +0100

    product detail HTML CSS Template

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -7,7 +7,8 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 10)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
-                                      coerce=int)
+                                      coerce=int,
+                                      label="")
     update = forms.BooleanField(required=False,
                                 initial=False,
                                 widget=forms.HiddenInput)
