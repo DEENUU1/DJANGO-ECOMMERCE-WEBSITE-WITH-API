211c110f9bb02bd6c2a78bf9bd1d40dbccf716c7
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2023-01-12 23:04:51 +0100

    Authenticated user cant go to login/register page

--- myshop/accounts/views.py
+++ myshop/accounts/views.py
@@ -3,19 +3,23 @@ from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
+
 # User registration view
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('/product_list/')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, f'{user} Twoje konto zostało utworzone')
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, f'{user} Twoje konto zostało utworzone')
 
-            return redirect('/accounts/login/')
+                return redirect('/accounts/login/')
 
     return render(request,
                   'accounts/register.html',
@@ -25,8 +29,30 @@ def registerPage(request):
 
 # User login view
 def loginPage(request):
-    context = {}
-    return render(request,
-                  'accounts/login.html',
-                  context)
+    if request.user.is_authenticated:
+        return redirect('/product_list/')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
+            user = authenticate(request,
+                                username=username,
+                                password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/product_list/')
+
+            else:
+                messages.info(request, 'Nazwa użytkownika lub hasło są nieprawidłowe')
+                return render(request,
+                              'accounts/login.html')
+
+    return render(request,
+                  'accounts/login.html', )
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/accounts/login/')
