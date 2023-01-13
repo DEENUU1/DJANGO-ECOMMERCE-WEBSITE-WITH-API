15595a67e3a95dd6808a32566057e18d1ceccb16
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2023-01-13 17:54:03 +0100

    returns page + applications files that I forgot to add to git

---
+++ myshop/shop_info/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# This view is to display page with information about shop
+def about(request):
+    return render(request, 'about.html')
+
+def statute(request):
+    return render(request, 'statute.html')
+
+def privacy_policy(request):
+    pass
+
+def shipping(request):
+    return render(request, 'shipping.html')
+
+def returns_complaints(request):
+    return render(request, 'returns.html')
+
+def faq(request):
+    pass
+
