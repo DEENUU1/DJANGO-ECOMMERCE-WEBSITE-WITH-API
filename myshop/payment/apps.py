c2335988de66fc647ab759d823ec0c0b97900a9e
Kacper Włodarczyk <111304236+DEENUU1@users.noreply.github.com>
2023-01-15 20:17:27 +0100

    Fixed the error with signals now after successfull payment order paid status change to value True #18

--- myshop/payment/apps.py
+++ myshop/payment/apps.py
@@ -2,7 +2,6 @@ from django.apps import AppConfig
 
 
 class PaymentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'payment'
     verbose_name = 'Payment'
     def ready(self):
