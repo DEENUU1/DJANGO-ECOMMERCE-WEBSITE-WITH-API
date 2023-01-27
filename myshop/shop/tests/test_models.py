8a0d4a27ea1971dabd7504c53638d3993a69603b
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-01-28 00:27:42 +0100

    Tests for category model

---
+++ myshop/shop/tests/test_models.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from shop.models import Category
+
+
+
+class TestCategoryModel(TestCase):
+
+    def setUp(self):
+        self.category1 = Category.objects.create(
+            name='klawiatury',
+            slug='klawiatury',
+        )
+
+    def test_name_slug(self):
+        self.assertEqual(self.category1.slug, 'klawiatury')
+
+
+    def test_name_str(self):
+        self.assertEqual(self.category1.name, 'klawiatury')
